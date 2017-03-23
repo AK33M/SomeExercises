e6d1acd97670ab24b5c183bbb44fe04756d612d4
Akeem Taiwo (Work) <akeem.taiwo@dataireland.ie>
2017-03-23 17:24:04 +0000

    Fibonacci Palindrome ReverseString ReverseInt

--- Fibonacci/Program.cs
+++ Fibonacci/Program.cs
@@ -10,6 +10,74 @@ namespace Fibonacci
     {
         static void Main(string[] args)
         {
+            ///Ask user for legth of series
+            string input = args[0];
+            string type = "";
+
+
+            int length = int.Parse(input);
+
+            if (length != 0)
+            {
+                switch (type)
+                {
+                    case "R":
+                        Console.WriteLine("Fibonacci Series By both Recursive method");
+                        for (int i = 1; i <= length; i++)
+                        {
+                            Console.Write(FibonacciByRecursion(i) + " ");
+                        }
+                        break;
+                    case "I":
+                        Console.WriteLine("Fibonacci Series By both Iterative method");
+                        for (int i = 1; i <= length; i++)
+                        {
+                            Console.Write(FibonacciByIteration(i) + " ");
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Fibonacci Series By both Recursive and Iterative methods");
+                        for (int i = 1; i <= length; i++)
+                        {
+                            Console.Write(FibonacciByRecursion(i) + " *");
+                        } 
+                        Console.WriteLine(" ");
+                        for (int i = 1; i <= length; i++)
+                        {
+                            Console.Write(FibonacciByRecursion(i) + " .");
+                        }
+                        break;
+                }
+            }
+        }
+
+        private static int FibonacciByRecursion(int number)
+        {
+            if (number == 1 || number == 2)
+            {
+                return 1;
+            }
+
+            return FibonacciByRecursion(number - 1) + FibonacciByRecursion(number - 2);
+        }
+
+        private static int FibonacciByIteration(int number)
+        {
+            if (number == 1 || number == 2)
+            {
+                return 1;
+            }
+
+            int numA = 1, numB = 1, numResult = 1;
+
+            for (int i = 3; i <= number; i++)
+            {
+                numResult = numA + numB;
+                numA = numB;
+                numB = numResult;
+            }
+
+            return numResult;
         }
     }
 }
---
+++ Palindrome/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool result = IsPalindrome("Akeem");
+            Console.WriteLine("Palindrome: " + result);
+            Console.WriteLine("**********");
+            result = IsPalindrome("Hannah");
+            Console.WriteLine("Palindrome: " + result);
+            Console.WriteLine("**********");
+            result = IsPalindrome("John");
+            Console.WriteLine("Palindrome: " + result);
+            Console.WriteLine("**********");
+            result = IsPalindrome(15693);
+            Console.WriteLine("Palindrome: " + result);
+            Console.WriteLine("**********");
+            result = IsPalindrome(159951);
+            Console.WriteLine("Palindrome: " + result);
+        }
+
+        private static bool IsPalindrome(int input)
+        {
+            var result = ReverseInt(input);
+            Console.WriteLine("Input is: " + input);
+            Console.WriteLine("Reverse is: " + result);
+            if (input == result)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private static bool IsPalindrome(string input)
+        {
+            var result = ReverseStringA(input);
+            Console.WriteLine("Input is: " + input);
+            Console.WriteLine("Reverse A is: " + result);
+            var resultb = ReverseStringB(input);
+            Console.WriteLine("Reverse B is: " + resultb);
+            var resultc = ReverseStringC(input);
+            Console.WriteLine("Reverse C is: " + resultc);
+            if (input.ToLower() == result.ToLower())
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private static string ReverseStringA(string input)
+        {
+            var reverse = new StringBuilder();
+            var charArray = input.ToCharArray();
+
+            for (int i = charArray.Length - 1; i >= 0; i--)
+            {
+                reverse.Append(charArray[i]);
+            }
+
+            return reverse.ToString();
+        }
+
+        private static string ReverseStringB(string input)
+        {
+            var reverse = new StringBuilder();
+            var charArray = input.ToCharArray();
+            var temp = '.';
+            var lastIndex = charArray.Length - 1;
+
+            for (int i = 0; i < charArray.Length/2; i++)
+            {
+                temp = charArray[lastIndex - i];
+                charArray[lastIndex - i] = charArray[i];
+                charArray[i] = temp;                
+            }
+
+            return reverse.Append(charArray).ToString();
+        }
+
+        private static string ReverseStringC(string input)
+        {
+            if (input.Length < 2)
+                return input;
+
+            return ReverseStringC(input.Substring(1)) + input.ToCharArray()[0];
+        }
+
+        private static int ReverseInt(int number)
+        {
+            int reverse = 0;
+
+            while (number != 0)
+            {
+                reverse = reverse * 10 + number % 10;
+                number = number / 10;
+            }
+
+            return reverse;
+        }
+    }
+}
